65f449e1c2748ac3fc291da894b1036c1e078bb6
drfiy <87424832+drfiy@users.noreply.github.com>
2022-01-02 23:40:34 +0300

    Add files via upload

---
+++ beta/tokens.py
@@ -0,0 +1,16 @@
+from types import Type
+
+
+class Token:
+    def __init__(self, exp, type):
+        self.exp = exp
+        self.type = type
+
+base_type = Type("base")
+
+base = {
+    "plus": Token("+", type=base_type),
+    "minus": Token("-", type=base_type),
+    "star": Token("*", type=base_type),
+    "slash": Token("/", type=base_type)
+}
