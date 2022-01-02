d8ec6888cf2736f48746d61705f33ed0ed613cdb
drfiy <87424832+drfiy@users.noreply.github.com>
2022-01-02 19:40:52 +0300

    Add files via upload

---
+++ beta/tokens.py
@@ -0,0 +1,25 @@
+class Type:
+    def __init__(self, name, regex=None):
+        self.name = name
+        self.regex = regex
+
+types = {
+    "num": Type("num", regex="\d*"),
+    "str": Type("str", regex='".*"'),
+    "bool": Type("bool", regex="(true|false)"),
+    "arrow": Type("arrow", regex="->"),
+    "object": Type("fun", regex="[a-z]"),
+    "operator": Type("base")
+}
+
+class Token:
+    def __init__(self, exp, type=types["operator"]):
+        self.exp = exp
+        seld.type = type
+
+operators = {
+    "plus": "+",
+    "minus": "-",
+    "star": "*",
+    "slash": "/",
+}
