55362c925b86e92ce0f355d9f823e507a7103dc0
drfiy <87424832+drfiy@users.noreply.github.com>
2022-01-04 19:35:27 +0300

    Update lexer.py

--- beta/lexer.py
+++ beta/lexer.py
@@ -1,17 +1,30 @@
+import re
+
+from tokens import Token
+from tokens import types
+
+
 class Lexer:
     def __init__(self, code, pos=0):
         text = "" # for catching tokens
-        self.tokens = []
+        self.exps = []
         
         while pos < len(code):
             if code[pos] == " " or code[pos] == "\n":
                 if len([sym for sym in text if sym == '"']) % 2 == 0:
-                    self.tokens.append(text)
+                    self.exps.append(text)
                     text = ""
                     pos += 1
             
             text += code[pos]
             pos += 1
         
-        self.tokens.append(text)
-
+        self.exps.append(text)
+        
+        
+        self.tokens = []
+        for exp in self.exps:
+            for type in types.values():
+                if re.match(type.regex, exp):
+                    self.tokens.append(Token(exp, type))
+                    break
