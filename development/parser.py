4eba7fb6b57223396e697a7b2b0c01ce591d3ae4
drfiy <87424832+drfiy@users.noreply.github.com>
2022-01-05 16:57:46 +0300

    Update parser.py

--- development/parser.py
+++ development/parser.py
@@ -1,18 +1,11 @@
-from lexer import Lexer
-
-
-builtins = {
-    "print": "print"
-}
-
-
-def sc_eval(tokens):
-    parse = {}
-    pos = 0
-    
-    while pos < len(tokens):
-        if tokens[pos].type.name == "arrow":
-            parse[tokens[pos-1]] = arguments[pos+1]
-        pos += 1
-    
-    return parse
+class Parser:
+    def __init__(self, tokens):
+        parse = {}
+        pos = 0
+        
+        while pos < len(tokens):
+            if tokens[pos].type.name == "arrow":
+                parse[tokens[pos-1].exp] = tokens[pos+1].exp
+            pos += 1
+        
+        self.parse = parse
