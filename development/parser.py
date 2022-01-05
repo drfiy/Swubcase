02d5ae3a4c698fba8f2fcdc689a65607c64b800f
drfiy <87424832+drfiy@users.noreply.github.com>
2022-01-05 16:07:50 +0300

    Update parser.py

--- development/parser.py
+++ development/parser.py
@@ -1,39 +1,21 @@
-# Bad code present
-
 import os
-
 import types
+
 from lexer import Lexer
 
 
 builtins = {
-    "print": "print",
-    "->": "("
-} # Not used
+    "print": "print"
+}
 
 
-def sceval(arguments):
-    parser = {}
-    index = 0
-    for argument in arguments:
-        if "->" in argument:
-            parser[arguments[index-1]] = arguments[index+1]
-        else:
-            index += 1
-    return parser
-        
-
-
-print("Swubcase 3.11 Beta")
-while True:
-    command = input(os.getcwd() + " $ ") # scrun test/test.sc
-
-    if command.endswith(".sc") and command.startswith("scrun "):
-        filepath = command.split("scrun ")[1]
-        with open(filepath, "r") as scfile:
-            lexer = Lexer(scfile.read())
-
-            args = []
-            for token in lexer.tokens:
-                args.append(token.exp)
-            print(sceval(args))
+def sc_eval(tokens):
+    parse = {}
+    pos = 0
+    
+    while pos < len(tokens):
+        if tokens[pos].type.name == "arrow":
+            parse[tokens[pos-1]] = arguments[pos+1]
+        pos += 1
+    
+    return parse
