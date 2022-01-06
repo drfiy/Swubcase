acc714b01eb0b4a832c9feaedb7f4f56665cacd2
drfiy <87424832+drfiy@users.noreply.github.com>
2022-01-06 15:45:47 +0300

    Update parser.py

--- development/parser.py
+++ development/parser.py
@@ -1,24 +1,21 @@
 class Parser:
     def __init__(self, tokens):
-        parse = {}
+        funs = {}
+        objs = {}
         pos = 0
 
         while pos < len(tokens):
             if tokens[pos].type.name == "arrow":
-                parse[tokens[pos-1].exp] = tokens[pos+1].exp
+                funs[tokens[pos-1].exp] = tokens[pos+1].exp
+            elif tokens[pos].type.name == "recogn":
+                objs[tokens[pos-1].exp] = tokens[pos+1].exp
             pos += 1
 
-        self.parse = parse
+        self.functions = funs
+        self.objects = objs
     
     def compile(self):
-        builtin = {
-            "print": "print"
-        }
-        
         funs = []
         
-        for fun, args in self.parse.items():
-            if fun in builtin.keys():
-                funs.append(f"{builtin[fun]}({args})")
-        
-        return [eval(fun) for fun in funs]
+        if self.compile() is not None:
+            ...
