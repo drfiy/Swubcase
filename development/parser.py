6d197a40ebe2837a041fb09d8c3dfc0f6f8ac7b2
drfiy <87424832+drfiy@users.noreply.github.com>
2022-01-07 17:22:37 +0300

    Update parser.py

--- development/parser.py
+++ development/parser.py
@@ -6,16 +6,29 @@ class Parser:
 
         while pos < len(tokens):
             if tokens[pos].type.name == "arrow":
-                funs[tokens[pos-1].exp] = tokens[pos+1].exp
+                funs[tokens[pos-1]] = tokens[pos+1]
             elif tokens[pos].type.name == "recogn":
-                objs[tokens[pos-1].exp] = tokens[pos+1].exp
+                objs[tokens[pos-1]] = tokens[pos+1]
             pos += 1
 
         self.functions = funs
         self.objects = objs
+        
+        self.objects["print"] = 'print("%T0")'
+        self.objects[""] = ''
+    
+    def _function(self, fun, args: list):
+        if fun.exp in self.objects:
+            fun = self.objects[fun.exp]
+            for i in range(len(args)):
+                fun = fun.replace("%T"+str(i), str(args[i]))
+        return fun
     
     def compile(self):
-        funs = []
-        
-        if self.compile() is not None:
-            ...
+        for fun, arg in self.functions.items():
+            if arg.type.name == "array":
+                arg = eval(arg.exp)
+            else:
+                print("error")
+            
+            eval(self._function(fun, arg))
